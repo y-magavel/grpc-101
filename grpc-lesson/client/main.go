6a5deb5e0f6d9959cc4ff9800394fde4001406cf
y-magavel <magavel.dev@gmail.com>
2024-01-15 22:22:30 +0900

    complete grpc lesson

---
+++ grpc-lesson/client/main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"grpc-lessson/pb"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+	callListFiles(client)
+	callDownload(client)
+	callUpload(client)
+	callUploadAndNotifyProgress(client)
+}
+
+func callListFiles(client pb.FileServiceClient) {
+	md := metadata.New(map[string]string{"authorization": "Bearer test-token"})
+	ctx := metadata.NewOutgoingContext(context.Background(), md)
+	res, err := client.ListFiles(ctx, &pb.ListFilesRequest{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(res.GetFilenames())
+}
+
+func callDownload(client pb.FileServiceClient) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	stream, err := client.Download(ctx, req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			resErr, ok := status.FromError(err) // grpcのエラーの場合okがtrueになる
+			if ok {
+				if resErr.Code() == codes.NotFound {
+					log.Fatalf("Error Code: %v, Error Message: %v", resErr.Code(), resErr.Message())
+				} else if resErr.Code() == codes.DeadlineExceeded {
+					log.Fatalln("deadline exceeded")
+				} else {
+					log.Fatalln("unknown grpc error")
+				}
+			} else {
+				log.Fatalln(err)
+			}
+		}
+
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(string): %s", string(res.GetData()))
+	}
+}
+
+func callUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "./storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.Upload(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf}
+		if sendErr := stream.Send(req); sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("received data size: %v", res.GetSize())
+}
+
+func callUploadAndNotifyProgress(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "./storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	stream, err := client.UploadAndNotifyProgress(context.Background())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// request
+	buf := make([]byte, 5)
+	go func() {
+		for {
+			n, err := file.Read(buf)
+			if n == 0 || err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			req := &pb.UploadAndNotifyProgressRequest{
+				Data: buf[:n],
+			}
+			if sendErr := stream.Send(req); sendErr != nil {
+				log.Fatalln(err)
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+
+		if err := stream.CloseSend(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	// response
+	ch := make(chan struct{})
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			log.Printf("received message: %v", res.GetMessage())
+		}
+		close(ch)
+	}()
+	<-ch
+}
